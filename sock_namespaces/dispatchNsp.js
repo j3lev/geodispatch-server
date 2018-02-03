21fb6cacfea085385136cefa2b331142fecc842c
Joseph Levenson <josephlevenson@Josephs-MBP.home>
2018-02-03 18:23:44 -0500

    Marker adding stuff

--- sock_namespaces/dispatchNsp.js
+++ sock_namespaces/dispatchNsp.js
@@ -1,5 +1,12 @@
 function namespace(socket) {
     console.log(socket.id + ' connected');
+    socket.on('ADD_MARKER', data => {
+        console.log('add marker request at', data);
+        // TODO publish to DB
+        setTimeout(() => {
+            socket.emit('ADD_MARKER_SUCCESS', data);
+        }, 3000);
+    });
 }
 
 module.exports = namespace;
